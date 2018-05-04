80878c65283f6a2ecf21937fcafa4d244552dc62
achliopa <athchliopanos@gmail.com>
2018-05-05 01:01:39 +0300

    last commit - done with course

--- my-folder/channels/main.go
+++ my-folder/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,16 +15,44 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+
+	// for i := 0; i < len(links); i++ {
+	// for {
+	// 	// fmt.Println(<-c)
+	// 	go checkLink(<-c, c)
+	// }
+
+	// alternative syntax
+	for l := range c {
+		// time.Sleep(5 * time.Second)
+		// go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, " might be down!")
+		// c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link, " is up!")
+	// c <- "Yes its up"
+	c <- link
 }
